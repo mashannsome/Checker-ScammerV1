de739953ed9195f82c7d64318140f3d4d65c668d
mashannsome <131888090+mashannsome@users.noreply.github.com>
2026-02-08 03:40:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 # Checker-Scammer API Configuration
 # Simpan Key Anda di sini
+# config.py
+# Solusi Alternatif OSINT
 
+API_KEY_ALL_IN_ONE = "d09b70de164dd9a847602278dcc0ef92b6e5a7bc8876040bb9537aebe769e1ea" 
+# Gunakan API dari penyedia seperti BinderByte/RapidAPI yang mencakup banyak fitur
 API_KEY_BINDERBYTE = "d09b70de164dd9a847602278dcc0ef92b6e5a7bc8876040bb9537aebe769e1ea"  # Untuk fitur SPX Tracking
 API_KEY_CEKPAJAK   = "MASUKKAN_KEY_DI_SINI"  # Untuk fitur Plat Kendaraan
 API_KEY_OSINT      = "MASUKKAN_KEY_DI_SINI"  # Untuk fitur Osint HP/Doxing
