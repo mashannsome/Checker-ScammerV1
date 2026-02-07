e57af437a0d69e0e63fc05d3d6f4f7df0f316362
mashannsome <131888090+mashannsome@users.noreply.github.com>
2026-02-08 04:15:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ API_KEY_ALL_IN_ONE = "d09b70de164dd9a847602278dcc0ef92b6e5a7bc8876040bb9537aebe7
 API_KEY_BINDERBYTE = "d09b70de164dd9a847602278dcc0ef92b6e5a7bc8876040bb9537aebe769e1ea"  # Untuk fitur SPX Tracking
 API_KEY_CEKPAJAK   = "MASUKKAN_KEY_DI_SINI"  # Untuk fitur Plat Kendaraan
 TRUECALLER_TOKEN      = "BzjqL8ac50570bbdd4075800aa1f6312fe26e"  # Untuk fitur Osint HP/Doxing
+EYECON_API_KEY = "41395f0f66mshef0186abccaf80fp112285jsnc2ac9c9c940419"
