0db8793b6efae06032db16aae54c9a6efb7f58c3
mashannsome <131888090+mashannsome@users.noreply.github.com>
2026-02-08 03:56:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ API_KEY_ALL_IN_ONE = "d09b70de164dd9a847602278dcc0ef92b6e5a7bc8876040bb9537aebe7
 # Gunakan API dari penyedia seperti BinderByte/RapidAPI yang mencakup banyak fitur
 API_KEY_BINDERBYTE = "d09b70de164dd9a847602278dcc0ef92b6e5a7bc8876040bb9537aebe769e1ea"  # Untuk fitur SPX Tracking
 API_KEY_CEKPAJAK   = "MASUKKAN_KEY_DI_SINI"  # Untuk fitur Plat Kendaraan
-API_KEY_OSINT      = "MASUKKAN_KEY_DI_SINI"  # Untuk fitur Osint HP/Doxing
+TRUECALLER_TOKEN      = "BzjqL8ac50570bbdd4075800aa1f6312fe26e"  # Untuk fitur Osint HP/Doxing
